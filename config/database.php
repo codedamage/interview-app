83a49427026c872184ab14f75fd3a7499024ba9e
codedamage <codedamage@protonmail.com>
2022-08-01 19:28:49 +0300

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url('postgres://kptemvnfaddaqj:558299f1eec94337c576d5077517a7ad566268b27fa400872150ad9bd726060b@ec2-44-206-214-233.compute-1.amazonaws.com:5432/d375k7uqc78d1b');
 return [
 
     /*
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => 'postgres://kptemvnfaddaqj:558299f1eec94337c576d5077517a7ad566268b27fa400872150ad9bd726060b@ec2-44-206-214-233.compute-1.amazonaws.com:5432/d375k7uqc78d1b',
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
